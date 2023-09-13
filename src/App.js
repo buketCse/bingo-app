f8066d5a81804a63b808e13afa18bac1050ed169
buket.algan <buket.algan@solvoyo.com>
2023-09-13 19:55:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bingo-App
+          This is Bingo-App
         </a>
       </header>
     </div>
